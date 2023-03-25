41ff57c21af88c7d673dc7fb83954d62486f70c7
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-03-25 16:46:24 +0530

    added routers to complete create api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,3 +1,4 @@
+const {response}=require('express');
 const {CityService} =require("../services/index");
 
 const cityService = new CityService();
@@ -23,14 +24,69 @@ const create =async (req,res) =>{
         })
     }    
 }
-const destroy =(req,res) =>{
+//DELETE
+const destroy = async(req,res) =>{
+    try {
+        const response= await cityService.deleteCity(req.params.id);
+        return res.status(200).json({
+            data:response,
+            success:true,
+            message:"Successfully deleted a city",
+            err:{}
 
+        })
+        }
+     catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success:false,
+            message:"Not able to delete a city",
+            err:error
+        })
+    }  
 }
-const get =(req,res) =>{
+const get =async (req,res) =>{
+    try {
+        const response= await cityService.getCity(req.params.id);
+        return res.status(201).json({
+            data:response,
+            success:true,
+            message:"Successfully fetched a city",
+            err:{}
 
+        })
+        }
+     catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success:false,
+            message:"Not able to get the city",
+            err:error
+        })
+    }  
 }
-const update =(req,res) =>{
+const update =async(req,res) =>{
+    try {
+        const response= await cityService.updateCity(req.params.id,req.body);
+        return res.status(201).json({
+            data:response,
+            success:true,
+            message:"Successfully updated a city",
+            err:{}
 
+        })
+        }
+     catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success:false,
+            message:"Not able to update a city",
+            err:error
+        })
+    }  
 }
 
 module.exports ={
