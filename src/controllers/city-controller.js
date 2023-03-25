c7e50285f75f55c2c03364fc73fa95d92af2c9dc
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-03-25 11:11:09 +0530

    Added City Controller

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,38 @@
+const {CityService} =require("../services/index");
+
+const cityService = new CityService();
+
+const create =async (req,res) =>{
+    try {
+        const city= await cityService.createCity(req.body);
+        return res.status(201).json({
+            data:city,
+            success:true,
+            message:"Successfully created a city",
+            err:{}
+
+        })
+        }
+     catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data: {},
+            success:false,
+            message:"Not able to create a city",
+            err:error
+        })
+    }    
+}
+const destroy =(req,res) =>{
+
+}
+const get =(req,res) =>{
+
+}
+const update =(req,res) =>{
+
+}
+
+module.exports ={
+    create,update,get,destroy
+} 
