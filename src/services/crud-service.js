6e9c27801e76447582e81ce7e225f8583e0b9b92
Harsh Balwani <balwaniharsh1001@gmail.com>
2023-04-09 01:17:46 +0530

    Added common crud classes for basic crud operation

---
+++ src/services/crud-service.js
@@ -0,0 +1,76 @@
+const { response } = require("express");
+
+class CrudService {
+    constructor(repository)
+    {
+        this.repository=repository;
+ 
+    }
+
+    async create(data){
+        try {
+            const response =await this.repository.create(data);
+            return response;
+        } catch (error) {
+            console.log("Something went wromg in Crud Service");
+            throw error;
+            
+        }
+    }
+
+    async destroy(id)
+    {
+        try {
+            const response=await this.repository.destroy(id);
+            return response;
+        } catch (error) {
+            console.log("Something went wromg in Crud Service");
+            throw error;
+            
+        }
+    }
+        
+    
+
+    async get(id)
+    {
+         try {
+            const response=await this.repository.get(id);
+            return response;
+            
+        } catch (error) {
+            console.log("Something went wromg in Crud Service");
+            throw error;
+            
+        }
+    }
+    
+
+    async getAll()
+    {
+        try {
+            const response=await this.repository.getAll();
+            return response;
+
+        } catch (error) {
+            console.log("Something went wromg in Crud Service");
+            throw error;
+            
+        }
+    }
+    
+
+    async update(id,data)
+    {
+        try {
+            const response=await this.repository.update(id,data);
+        } catch (error) {
+            console.log("Something went wromg in Crud Service");
+            throw error;
+            
+        }
+    }
+
+}
+
+module.exports= CrudService;
